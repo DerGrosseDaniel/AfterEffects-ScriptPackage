8e22b41091fd05ba93b430900c524ce105c8a473
DerGrosseDaniel <weg4ever+github@googlemail.com>
2015-07-18 00:37:36 +0200

    Update PositionAndOrientationToFile.jsx

--- PositionAndOrientationToFile.jsx
+++ PositionAndOrientationToFile.jsx
@@ -1,6 +1,23 @@
 {
+
     app.beginUndoGroup("Position and Orientation to File");
-         var selectedLayers = app.project.activeItem.selectedLayers;
+
+var frameLength = 1/app.project.activeItem.frameRate;
+
+         var target = app.project.activeItem.selectedLayers[0];
+
+          var  targetIn = target.inPoint
+        var  targetOut = target.outPoint
          
+         var output = "";
+         var test = "hjsdf"
+         
+         for ( i = targetIn; i<= targetOut; i += frameLength){
+               output = output + target.orientation.valueAtTime(i,true)
+             }
+         
+         alert(output)
+
     app.endUndoGroup();
- }
+ 
+}
